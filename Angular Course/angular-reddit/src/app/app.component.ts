c8517a7db7c88a6d0903a036a7e56f47e336f785
ilyaad1991 <ilyaad1991@gmai>
2018-09-17 01:23:02 +0400

    Move reddit project

---
+++ Angular Course/angular-reddit/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Article } from '../app/article/article.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  articles: Article[];
+
+  constructor(){
+
+    this.articles = [new Article('test', 'test', 10), new Article('test', 'test', 11), ];
+
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 80));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+}
